dfc4695596187e938328f2da534732695b71ee44
Henry <junyanhenry@gmail.com>
2023-05-05 23:31:25 -0400

    多多多

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # sharded-kv-cache
+
+## TODO (option1): 
+- [ ] Decide on new Get, Set, Delete grpc APIs. 
+- [ ] Write new shardkv server, which is a wrapper of lab3 raft server.
+- [ ] The manipulations of shards are done through rf.applyLogs() ? 
+- [ ] how to deal with TTLs when a server goes down and come back up after ttl is expired =>  self-clean? 
+- [ ] how to implement "copy data for a live shard to another node"? Communication between nodes should be lab3 RPC, not grpc. 
+- [ ] worry about shardmaplistener later
+
+## TODO (option2):
+- [ ] Write new shardkv server, which is a wrapper of lab3 raft server.
+- [ ] Write new shardkv client, which talks with shardkv server in lab3 rpc. 
+- [ ] Extend delete, ttl, shardmaplistener, clientpool??? 
--- kv/server.go
+++ kv/server.go
@@ -1,6 +1,70 @@
 package kv
 
 import (
-
+	"sync"
+	"cs426.yale.edu/final/raft"
+	"time"
+	"log"
 )
+
+const Debug = 0
+
+func DPrintf(format string, a ...interface{}) (n int, err error) {
+	if Debug > 0 {
+		log.Printf(format, a...)
+	}
+	return
+}
+
+type kvObject struct {
+	value       string
+	stored_time time.Time
+	ttlMs       int64
+}
+
+type kvShard struct {
+	data   map[string]*kvObject
+	mu     sync.RWMutex
+	shutCh chan bool
+}
+
+func MakeKvShard() *kvShard {
+	shard := kvShard{data: make(map[string]*kvObject), shutCh: make(chan bool)}
+	go shard.ttlMonitor()
+	return &shard
+}
+
+
+func (shard *kvShard) ttlMonitor() {
+	for {
+		select {
+		case <-shard.shutCh:
+			return
+		case <-time.After(time.Second * 1):
+			shard.cleanup()
+		}
+	}
+}
+
+type KVServer struct {
+	mu      sync.Mutex
+	me      int
+	rf      *raft.Raft
+	applyCh chan raft.ApplyMsg
+	stopCh  chan struct{}
+
+	// TODO: deal with the case when there are way too many logs
+	shardMap   *ShardMap
+	storage     map[string]*kvObject
+	ttlShutdown chan bool
+	shards map[int32]*kvShard
+
+	persister      *raft.Persister
+	lastApplyIndex int
+	lastApplyTerm  int
+
+	lockStart time.Time // debug 找出长时间 lock
+	lockEnd   time.Time
+	lockName  string
+}
 
